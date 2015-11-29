4bbce22093879747ed256fd1dac9cd372e11c01c
U-DANIEL-DESKTOP\Daniel <daniel.blanche@usask.ca>
2015-11-28 19:50:56 -0600

    Code now fails gracefully when can't connect Rather than the crashing hopelessly thing that was happening

--- XamarinClient/MP_Chess/MP_Chess/SocketSingleton.cs
+++ XamarinClient/MP_Chess/MP_Chess/SocketSingleton.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-
-using Java.IO;
-using Java.Net;
-
 using System.Net;
 using System.Net.Sockets;
 using System.IO;
@@ -15,18 +11,20 @@ namespace MP_Chess
 		private static SocketSingleton instance;
 
 		public static int SERVER_PORT;
-		public static String SERVER_IP;
+		public static string SERVER_IP;
 
 		public static TcpClient connection;
 
 		public static StreamWriter outWriter;
+
+		public static bool connected;
 
 		public static StreamReader outReader;
 
 		public SocketSingleton(){
 		}
 
-		public SocketSingleton(String serverAddr, int serverPort){
+		public SocketSingleton(string serverAddr, int serverPort){
 			if (serverAddr != null && serverPort != 0) {
 				SERVER_IP = serverAddr;
 				SERVER_PORT = serverPort;
@@ -43,9 +41,11 @@ namespace MP_Chess
 			if (instance == null) {
 				instance = new SocketSingleton ();
 				try{
-					if(connection == null) instance.startSocket();
+					if(connection == null)
+						instance.startSocket();
 					outWriter = new StreamWriter(connection.GetStream());
 					outReader = new StreamReader(connection.GetStream());
+
 
 				//	clientSocket = new Java.Net.Socket(SERVER_IP,SERVER_PORT);
 				}catch(System.IO.IOException e){
@@ -56,17 +56,34 @@ namespace MP_Chess
 		}
 
 		public bool isConnected(){
-			if (connection != null)
-				return true;
-			return false;
+			/*
+			// Detect if client disconnected
+			if( connection.Client.Poll( 0, SelectMode.SelectRead ) )
+			{
+				byte[] b = new byte[1];
+				if (connection.Client.Receive (b, SocketFlags.Peek) == 0) {
+					// Client disconnected
+					return true;
+				} else {
+					return false;
+				}
+			}
+			return false; */
+			return connected;
 		}
 
 		public void startSocket(){
 		try{
 			if (connection == null)
-				if(SERVER_IP != null && SERVER_PORT > 0)
-					connection = new TcpClient (SERVER_IP, SERVER_PORT);
-
+				if(SERVER_IP != null && SERVER_PORT > 0){
+					connection = new TcpClient ();
+					var result = connection.BeginConnect(SERVER_IP, SERVER_PORT, null, null);
+					var done = result.AsyncWaitHandle.WaitOne(System.TimeSpan.FromSeconds(1));
+					if(!done)
+						connected = false;
+					else
+						connected = true;
+				}
 		//			clientSocket = new Java.Net.Socket(SERVER_IP,SERVER_PORT);
 		//		outWriter = new StreamWriter(connection.GetStream());
 			}catch(Exception e){
