dbe625bb9c4f9d66a1b61af6e1acabbf76d4f3f0
U-DANIEL-DESKTOP\Daniel <daniel.blanche@usask.ca>
2015-12-18 10:40:10 -0600

    Added compiled binary and apk that can be installed on android

--- XamarinClient/MP_Chess/MP_Chess/obj/Debug/android/src/mono/MonoPackageManager.java
+++ XamarinClient/MP_Chess/MP_Chess/obj/Debug/android/src/mono/MonoPackageManager.java
@@ -85,5 +85,5 @@ class MonoPackageManager_Resources {
 	};
 	public static final String[] Dependencies = new String[]{
 	};
-	public static final String ApiPackageName = "Mono.Android.Platform.ApiLevel_19";
+	public static final String ApiPackageName = null;
 }
